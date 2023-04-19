ffe1bdff248f7af8243400dabe6bdcde280da027
sosso <cypossosmu51c@gmail.com>
2023-04-19 15:16:03 +0200

    Worked on layout

--- JS/animate-menu.js
+++ JS/animate-menu.js
@@ -1,14 +1,27 @@
+let menu = document.querySelector("menu");
+let nav = document.querySelector("nav ul");
+
 addEventListener("scroll", (e) => {
     if (innerWidth < 1080) {
-        let menu = document.querySelector("menu");
-        
-        if (menu.getBoundingClientRect().top == 0) {
+        if (menu.getBoundingClientRect().top <= 0) {
             setTimeout(() => {
                 menu.style.height = "100px";
             }, 300);
         } else {
             setTimeout(() => {
                 menu.style.height = "150px";
+            }, 300);
+        }
+    } else {
+        if (menu.getBoundingClientRect().top <= 0) {
+            setTimeout(() => {
+                nav.style.top = "50%";
+                nav.style.transform = "translateY(-50%)";
+            }, 300);
+        } else {
+            setTimeout(() => {
+                nav.style.top = "0";
+                nav.style.transform = "translateY(0)";
             }, 300);
         }
     }
