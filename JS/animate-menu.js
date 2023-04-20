8367e7e8536501f335e831fb0a38ac19a5efc766
sosso <cypossosmu51c@gmail.com>
2023-04-20 15:48:53 +0200

    Fixed animatable menu

--- JS/animate-menu.js
+++ JS/animate-menu.js
@@ -1,6 +1,19 @@
 let menu = document.querySelector("menu");
 let nav = document.querySelector("nav ul");
 
+addEventListener("resize", (e) => {
+    if (innerWidth < 1080) {
+        menu.style.width = "100%";
+        if (menu.getBoundingClientRect().top <= 0)
+            menu.style.height = "100px";
+        else
+            menu.style.height = "150px";
+    } else {
+        menu.style.height = "100vh";
+        menu.style.width = "200px";
+    }
+})
+
 addEventListener("scroll", (e) => {
     if (innerWidth < 1080) {
         if (menu.getBoundingClientRect().top <= 0) {
